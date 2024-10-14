73018cbb0679d99ee4e288cc5f035b72d80a8395
mohamadalabsi <absimoda1@gmail.com>
2024-10-14 23:20:31 +0200

    Spring Web Update and DELETE

--- src/main/java/org/me/learning/springmvc/controller/ProductController.java
+++ src/main/java/org/me/learning/springmvc/controller/ProductController.java
@@ -4,11 +4,11 @@ package org.me.learning.springmvc.controller;
 
 //the is responsible to accept the request for the products
 
+import org.apache.catalina.Service;
 import org.me.learning.springmvc.model.Product;
 import org.me.learning.springmvc.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,14 +17,28 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
-
-
-    @RequestMapping("/products")
+//?     the data will be converted from Object to json and the other way around automatically using the library jackson
+//!     GET
+    @RequestMapping("/products")   // by default this annotation will use GET method
     public List<Product> getProducts(){
         return productService.getProduct();
     }
 
 
+//     searching about specific ID
+//    @RequestMapping("/products/{prodID}") // to make it dynamic
+    @GetMapping("/products/{prodID}")
+    public Product getProductByID(@PathVariable int prodID){ //you have to use this annotation because we want to match it now
+         return productService.getproductByID(prodID);
+    }
+
+//!    POST
+
+//    @RequestMapping()
+    @PostMapping ("/products")
+   public void  addProduct(@RequestBody Product product){ // we use RequestBody if u send a body from the client to the server
+        productService.addProduct(product);
+   }
 
 
 }
--- src/main/java/org/me/learning/springmvc/service/ProductService.java
+++ src/main/java/org/me/learning/springmvc/service/ProductService.java
@@ -12,9 +12,27 @@ public class ProductService {
 
 //    we need a methode that return a list of products
 
-    List<Product> products = Arrays.asList(new Product(101,"Iphone",4000),new Product(102,"Canon",6600),new Product(103,"AC",20000));
+//    asList is limited list
+//    List<Product> products = Arrays.asList(new Product(101,"Iphone",4000),new Product(102,"Canon",6600),new Product(103,"AC",20000));
+      List<Product> products = new ArrayList<>( Arrays.asList(new Product(101,"Iphone",4000),new Product(102,"Canon",6600),new Product(103,"AC",20000)));
 
+
+
+//!    now getting product = get
     public List < Product> getProduct() {
         return products;
     }
+
+    public Product getproductByID( int prodID) {
+      return   products.stream().filter( p->  p.getProdID() ==prodID)
+              .findFirst().get();
+//                        .orElse(new Product(104,"samsung AC",40540)): if you provide id that does not exist you can handel it this way , but not the best way
+
+    }
+
+//!    now adding product = post
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
+
 }
